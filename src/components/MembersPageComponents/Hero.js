e547e54e23e24bd84e8e15cfb53b48a9a3203a69
Albert Chang <a89529294@hotmail.com>
2021-03-31 14:09:03 +0800

    .

--- src/components/MembersPageComponents/Hero.js
+++ src/components/MembersPageComponents/Hero.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
+  '@keyframes blinker': {
+    '50%': {
+      opacity: 0,
+    },
+  },
   heroTextContainer: {
     ...theme.sectionPadding,
     [theme.breakpoints.only('xs')]: {
@@ -19,15 +24,68 @@ const useStyles = makeStyles((theme) => ({
   typedText: {
     color: theme.palette.common.darkLogoRed,
   },
+  blinkingText: {
+    animation: '$blinker 700ms linear infinite',
+  },
 }));
 
 const HeroText = () => {
   const classes = useStyles();
-  const typedTextArray = [
-    ['多', '金'],
-    ['高', '學', '歷'],
-  ];
-  const [typedTextState, setTypedTextState] = [];
+  const [strArrIdx, setStrArrIdx] = useState(0);
+  const [strIdx, setStrIdx] = useState(0);
+  const myRef = useRef();
+  const strArrIdxRef = useRef();
+
+  const strArray = ['高富帥', '幽默風趣'];
+
+  useEffect(() => {
+    const pauseInterval = 2000;
+    const forwardInterval = 200;
+    const backwardInterval = 50;
+    let interval = forwardInterval;
+    let direction = 'forward';
+
+    const timeout = () => {
+      setTimeout(() => {
+        setStrIdx((i) => {
+          console.log(strArrIdx);
+          if (i === strArray[strArrIdxRef.current].length) {
+            interval = backwardInterval;
+            direction = 'backward';
+            myRef.current.classList.remove(classes.blinkingText);
+            return --i;
+          } else {
+            if (i === 0 && direction === 'backward') {
+              interval = forwardInterval;
+              direction = 'forward';
+
+              setStrArrIdx((i) => {
+                if (i === strArray.length - 1) return 0;
+                else return ++i;
+              });
+
+              return ++i;
+            } else if (direction === 'forward') {
+              if (i === strArray[strArrIdxRef.current].length - 1) {
+                myRef.current.classList.add(classes.blinkingText);
+                interval = pauseInterval;
+              }
+
+              return ++i;
+            } else {
+              return --i;
+            }
+          }
+        });
+        timeout();
+      }, interval);
+    };
+    timeout();
+  }, []);
+
+  useEffect(() => {
+    strArrIdxRef.current = strArrIdx;
+  }, [strArrIdx]);
 
   return (
     <Grid item xs={12} md={6} className={classes.heroTextContainer}>
@@ -35,11 +93,16 @@ const HeroText = () => {
         <Grid item xs={12}>
           <Typography variant="h2" color="textPrimary">
             我心目中得理想情人擁有 <br />
-            <span className={classes.typedText}></span>
+            <span className={classes.typedText}>
+              {strArray[strArrIdx].substr(0, strIdx)}
+            </span>
+            <span className={classes.typedText} ref={myRef}>
+              |
+            </span>
           </Typography>
         </Grid>
         <Grid item xs={12}>
-          <Typography variant=""></Typography>
+          {/* <Typography variant=""></Typography> */}
         </Grid>
       </Grid>
     </Grid>
